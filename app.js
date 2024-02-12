f0510dd692af3e9e5e182f04d6f5c2dcb14b8461
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2024-02-12 22:52:45 +0530

    premium-account-features

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const sequelize = require("./db/db");
-const { authRoutes } = require("./Routes/AuthRoutes");
+const path = require('path');
+
+const express = require('express');
+var cors = require('cors')
+const sequelize = require('./util/database');
+const User = require('./models/users');
+const Expense= require("./models/expenses")
+const Order = require('./models/orders');
+
+const userRoutes = require('./routes/user')
+const expenseRoutes = require('./routes/expense')
+const purchaseRoutes = require('./routes/purchase')
+const premiumFeatureRoutes = require('./routes/premiumFeature')
 
 const app = express();
+const dotenv = require('dotenv');
 
-app.use(express.json());
+// get config vars
+dotenv.config();
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(authRoutes);
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("<h1>hello</h1>");
-});
+// app.use(bodyParser.urlencoded());  ////this is for handling forms
+app.use(express.json());  //this is for handling jsons
 
-sequelize
-  .sync()
-  .then(() => {
-    console.log("connected");
-    app.listen(4500);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+app.use('/user', userRoutes)
+app.use('/expense', expenseRoutes)
+app.use('/purchase', purchaseRoutes)
+app.use('/premium', premiumFeatureRoutes)
+
+User.hasMany(Expense);
+Expense.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
+sequelize.sync()
+    .then(() => {
+        app.listen(4500);
+    })
+    .catch(err => {
+        console.log(err);
+    })
